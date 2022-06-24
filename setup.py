1702d8ffbeb739ccbe0a83344520aef3a53b8122
Daniele Jahier Pagliari <daniele.jahier@polito.it>
2022-06-24 18:38:40 +0200

    fixed setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='flexnas',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    install_requires=['setuptools'],
+    maintainer='Daniele Jahier Pagliari',
+    maintainer_email='daniele.jahier@polito.it',
+    description='A simple library for lightweight NAS based on PyTorch',
+    license='TODO',
+    tests_require=['unittest'],
+    python_requires=">=3.7",
+)
