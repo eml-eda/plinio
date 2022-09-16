8befbd71ae1a73b2eb14a4fd19bd848d8816503e
Pietro Borgaro <pietro.borgaro@gmail.com>
2022-09-16 17:18:11 +0200

    Fixed named_nas_parameters, forward and get_size

--- flexnas/methods/supernet/supernet_module.py
+++ flexnas/methods/supernet/supernet_module.py
@@ -13,13 +13,8 @@ class SuperNetModule(nn.Module):
         self.n_layers = len(self.input_layers)
         self.n_parameters = torch.zeros(1)
 
-        # self.alpha = nn.Parameter()
-        self.alpha = (1 / self.n_layers) * torch.ones(self.n_layers, dtype=torch.float)
-
-        for i, layer in enumerate(self.input_layers):
-            for param in layer.parameters():
-                prod = torch.prod(torch.tensor(param.shape))
-                self.n_parameters += self.alpha[i] * prod
+        self.alpha = nn.Parameter(
+            (1 / self.n_layers) * torch.ones(self.n_layers, dtype=torch.float), requires_grad=True)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         """Forward function for the SuperNetModule that returns a weighted
@@ -31,10 +26,14 @@ class SuperNetModule(nn.Module):
         :rtype: torch.Tensor
         """
 
+        softmax = nn.Softmax(dim=0)
+        soft_alpha = softmax(self.alpha)
+
         y = []
         for i, layer in enumerate(self.input_layers):
-            y.append(self.alpha[i] * layer(input))
-        y = sum(y)
+            y.append(soft_alpha[i] * layer(input))
+        y = torch.stack(y, dim=0).sum(dim=0)
+
         return y
 
     def export(self) -> nn.Module:
@@ -56,12 +55,16 @@ class SuperNetModule(nn.Module):
         :return: number of weights of the module (weighted sum)
         :rtype: torch.Tensor
         """
+        for i, layer in enumerate(self.input_layers):
+            for param in layer.parameters():
+                prod = torch.prod(torch.tensor(param.shape))
+                self.n_parameters += self.alpha[i] * prod
 
         return self.n_parameters
 
     def named_nas_parameters(
             self, prefix: str = '', recurse: bool = False) -> Iterator[Tuple[str, nn.Parameter]]:
-        """Returns an iterator over the architectural parameters of all layers in the module, yielding
+        """Returns an iterator over the architectural parameters of this module, yielding
         both the name of the parameter as well as the parameter itself
 
         :param prefix: prefix to prepend to all parameter names, defaults to ''
@@ -74,14 +77,12 @@ class SuperNetModule(nn.Module):
 
         prfx = prefix
         prfx += "." if len(prefix) > 0 else ""
+        prfx += "alpha"
 
-        for layer in self.input_layers:
-            for name, param in layer.named_parameters(
-                    prfx + layer.__class__.__name__, recurse):
-                yield name, param
+        yield prfx, self.alpha
 
     def nas_parameters(self, recurse: bool = False) -> Iterator[nn.Parameter]:
-        """Returns an iterator over the architectural parameters of all layers in the module
+        """Returns an iterator over the architectural parameters of this module
 
         :param recurse: _description_, defaults to False
         :type recurse: bool, optional
