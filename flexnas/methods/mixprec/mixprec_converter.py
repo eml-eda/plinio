Fixed "fake batch" size for shape propagation that was causing an error in MACs computation.
One test fails on PITLinear but this seems unrelated.

flexnas/methods/mixprec/mixprec_converter.py
@@ -99,8 +99,8 @@ def convert(model: nn.Module,
     mod = fx.GraphModule(tracer.root, graph, name)
 
     # Shape Prop
-    # create a "fake" minibatch of 32 inputs for shape prop
-    batch_example = torch.stack([torch.rand(input_shape)] * 32, 0)
+    # create a "fake" minibatch of 1 input for shape prop
+    batch_example = torch.stack([torch.rand(input_shape)] * 1, 0)
     # TODO: this is not very robust. Find a better way
     device = next(model.parameters()).device
     ShapeProp(mod).propagate(batch_example.to(device))

flexnas/methods/pit/graph.py
@@ -88,8 +88,8 @@ def convert(model: nn.Module, input_shape: Tuple[int, ...], conversion_type: str
     graph = tracer.trace(model.eval())
     name = model.__class__.__name__
     mod = fx.GraphModule(tracer.root, graph, name)
-    # create a "fake" minibatch of 32 inputs for shape prop
-    batch_example = torch.stack([torch.rand(input_shape)] * 32, 0)
+    # create a "fake" minibatch of 1 input for shape prop
+    batch_example = torch.stack([torch.rand(input_shape)] * 1, 0)
     device = next(model.parameters()).device
     ShapeProp(mod).propagate(batch_example.to(device))
     add_node_properties(mod)

flexnas/methods/pit_supernet/graph.py
@@ -58,7 +58,7 @@ def convert(model: nn.Module, input_shape: Tuple[int, ...], conversion_type: str
     graph = tracer.trace(model.eval())
     name = model.__class__.__name__
     mod = fx.GraphModule(tracer.root, graph, name)
-    batch_example = torch.stack([torch.rand(input_shape)] * 32, 0)
+    batch_example = torch.stack([torch.rand(input_shape)] * 1, 0)
     device = next(model.parameters()).device
     ShapeProp(mod).propagate(batch_example.to(device))
     add_node_properties(mod)