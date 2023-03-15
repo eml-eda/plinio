Fixed input features calculator for MixPrec in case of 0-bit precision

plinio/methods/mixprec/graph.py
@@ -25,11 +25,14 @@ from torch.fx.passes.shape_prop import ShapeProp
 
 from plinio.methods.mixprec.nn import MixPrec_Linear, MixPrec_Conv2d, \
     MixPrecModule
-from plinio.graph.annotation import add_node_properties
+from plinio.graph.annotation import add_features_calculator, add_node_properties, \
+    associate_input_features
+from plinio.graph.inspection import is_inherited_layer
 from plinio.methods.mixprec.nn.mixprec_qtz import MixPrecType, MixPrec_Qtz_Layer
 from plinio.methods.mixprec.quant.quantizers import Quantizer
 from plinio.graph import get_graph_outputs
 from plinio.graph import inspection
+from plinio.graph.features_calculation import ModAttrFeaturesCalculator
 
 # add new supported layers here:
 mixprec_layer_map: Dict[Type[nn.Module], Type[MixPrecModule]] = {
@@ -115,6 +118,10 @@ def convert(model: nn.Module,
                                    exclude_names,
                                    exclude_types)
 
+    if conversion_type in ('autoimport', 'import'):
+        add_features_calculator(mod, [mixprec_features_calc])
+        associate_input_features(mod)
+        register_input_features(mod)
     mod.graph.lint()
     mod.recompile()
     return mod, target_layers
@@ -465,3 +472,30 @@ def fuse_conv_bn(mod: fx.GraphModule):
             # safely remove the batch norm.
             mod.graph.erase_node(node)
     mod.delete_all_unused_submodules()
+
+
+def register_input_features(mod: fx.GraphModule):
+    for n in mod.graph.nodes:
+        if is_inherited_layer(n, mod, (MixPrecModule,)):
+            sub_mod = cast(MixPrecModule, mod.get_submodule(str(n.target)))
+            fc = n.meta['input_features_set_by'].meta['features_calculator']
+            sub_mod.input_features_calculator = fc
+
+
+def mixprec_features_calc(n: fx.Node, mod: fx.GraphModule) -> Optional[ModAttrFeaturesCalculator]:
+    """Sets the feature calculator for a MixPrec Module
+
+    :param n: node
+    :type n: fx.Node
+    :param mod: the parent module
+    :type mod: fx.GraphModule
+    :return: optional feature calculator object for PIT node
+    :rtype: ModAttrFeaturesCalculator
+    """
+    if is_inherited_layer(n, mod, (MixPrecModule,)):
+        # For PIT NAS-able layers, the "active" output features are stored in the
+        # out_features_eff attribute, and the binary mask is in features_mask
+        sub_mod = mod.get_submodule(str(n.target))
+        return ModAttrFeaturesCalculator(sub_mod, 'out_features_eff', 'features_mask')
+    else:
+        return None

plinio/methods/mixprec/nn/mixprec_conv2d.py
@@ -26,6 +26,7 @@ from ..quant.nn import Quant_Conv2d, Quant_List
 from .mixprec_module import MixPrecModule
 from .mixprec_qtz import MixPrecType, MixPrec_Qtz_Layer, MixPrec_Qtz_Channel, \
     MixPrec_Qtz_Layer_Bias, MixPrec_Qtz_Channel_Bias
+from plinio.graph.features_calculation import ConstFeaturesCalculator, FeaturesCalculator
 
 
 class MixPrec_Conv2d(nn.Conv2d, MixPrecModule):
@@ -99,6 +100,8 @@ class MixPrec_Conv2d(nn.Conv2d, MixPrecModule):
             self.mixprec_b_quantizer = lambda *args: None  # Do Nothing
 
         self.w_mixprec_type = w_mixprec_type
+        # this will be overwritten later when we process the model graph
+        self._input_features_calculator = ConstFeaturesCalculator(conv.in_channels)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         """The forward function of the mixed-precision NAS-able layer.
@@ -132,7 +135,7 @@ class MixPrec_Conv2d(nn.Conv2d, MixPrecModule):
         with torch.no_grad():
             for quantizer in [self.mixprec_w_quantizer, self.mixprec_a_quantizer]:
                 if isinstance(quantizer, (MixPrec_Qtz_Layer, MixPrec_Qtz_Channel)):
-                    quantizer.temperature = torch.tensor(value, dtype = torch.float32)
+                    quantizer.temperature = torch.tensor(value, dtype=torch.float32)
 
     def update_softmax_options(self, gumbel_softmax, hard_softmax, disable_sampling):
         """Set the flags to choose between the softmax, the hard and soft Gumbel-softmax
@@ -491,23 +494,13 @@ class MixPrec_Conv2d(nn.Conv2d, MixPrecModule):
         :rtype: torch.Tensor
         """
         eff_w_prec = self.mixprec_w_quantizer.effective_precision
-        cost = self.in_channels * self.out_channels * self.kernel_size[0] * self.kernel_size[1]
-        cost = cost / self.groups
+        cout = self.out_features_eff
+        cin = self.input_features_calculator.features
+        cost = cin * cout * self.kernel_size[0] * self.kernel_size[1]
+        if self.groups > 1:
+            cost = cost / cout
         eff_cost = cost * eff_w_prec
         return eff_cost
-
-    def get_size_binarized(self) -> torch.Tensor:
-        """Method that computes the number of weights for the layer considering
-        binarized masks
-
-        :return: the actual memory occupation of weights
-        :rtype: torch.Tensor
-        """
-        w_prec = torch.tensor(self.selected_w_precision).sum() / self.out_channels
-        cost = self.in_channels * self.out_channels * self.kernel_size[0] * self.kernel_size[1]
-        cost = cost / self.groups
-        tot_cost = cost * w_prec
-        return tot_cost
 
     # N.B., EdMIPS formulation
     def get_macs(self) -> torch.Tensor:
@@ -519,3 +512,38 @@ class MixPrec_Conv2d(nn.Conv2d, MixPrecModule):
         eff_a_prec = self.mixprec_a_quantizer.effective_precision
         eff_cost = self.get_size() * self.out_height * self.out_width * eff_a_prec
         return eff_cost
+
+    @property
+    def out_features_eff(self) -> torch.Tensor:
+        """Returns the number of not pruned channels for this layer.
+
+        :return: the number of not pruned channels for this layer.
+        :rtype: torch.Tensor
+        """
+        if self.w_mixprec_type == MixPrecType.PER_CHANNEL:
+            return cast(torch.Tensor, self.mixprec_w_quantizer.out_features_eff)
+        else:
+            return cast(torch.Tensor, self.out_channels)
+
+    @property
+    def input_features_calculator(self) -> FeaturesCalculator:
+        """Returns the `FeaturesCalculator` instance that computes the number of input features for
+        this layer.
+
+        :return: the `FeaturesCalculator` instance that computes the number of input features for
+        this layer.
+        :rtype: FeaturesCalculator
+        """
+        return self._input_features_calculator
+
+    @input_features_calculator.setter
+    def input_features_calculator(self, calc: FeaturesCalculator):
+        """Set the `FeaturesCalculator` instance that computes the number of input features for
+        this layer.
+
+        :param calc: the `FeaturesCalculator` instance that computes the number of input features
+        for this layer
+        :type calc: FeaturesCalculator
+        """
+        calc.register(self)
+        self._input_features_calculator = calc

plinio/methods/mixprec/nn/mixprec_linear.py
@@ -27,6 +27,7 @@ from ..quant.nn import Quant_Linear, Quant_List
 from .mixprec_module import MixPrecModule
 from .mixprec_qtz import MixPrecType, MixPrec_Qtz_Layer, MixPrec_Qtz_Channel, \
     MixPrec_Qtz_Layer_Bias, MixPrec_Qtz_Channel_Bias
+from plinio.graph.features_calculation import ConstFeaturesCalculator, FeaturesCalculator
 
 
 class MixPrec_Linear(nn.Linear, MixPrecModule):
@@ -82,6 +83,8 @@ class MixPrec_Linear(nn.Linear, MixPrecModule):
             self.mixprec_b_quantizer = lambda *args: None  # Do Nothing
 
         self.w_mixprec_type = w_mixprec_type
+        # this will be overwritten later when we process the model graph
+        self._input_features_calculator = ConstFeaturesCalculator(linear.in_features)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         """The forward function of the mixed-precision NAS-able layer.
@@ -103,9 +106,6 @@ class MixPrec_Linear(nn.Linear, MixPrecModule):
 
         # Linear operation
         q_out = F.linear(q_inp, q_w, q_b)
-
-        # TODO: Understand what I need to do for regularization
-        # Save info for regularization
 
         return q_out
 
@@ -466,18 +466,9 @@ class MixPrec_Linear(nn.Linear, MixPrecModule):
         :rtype: torch.Tensor
         """
         eff_w_prec = self.mixprec_w_quantizer.effective_precision
-        cost = self.in_features * self.out_features * eff_w_prec
-        return cost
-
-    def get_size_binarized(self) -> torch.Tensor:
-        """Method that computes the number of weights for the layer considering
-        binarized masks
-
-        :return: the actual memory occupation of weights
-        :rtype: torch.Tensor
-        """
-        w_prec = torch.tensor(self.selected_w_precision).sum() / self.out_features
-        cost = w_prec * self.in_features * self.out_features
+        cout = self.out_features_eff
+        cin = self.input_features_calculator.features
+        cost = cin * cout * eff_w_prec
         return cost
 
     # N.B., EdMIPS formulation
@@ -491,3 +482,38 @@ class MixPrec_Linear(nn.Linear, MixPrecModule):
         eff_a_prec = self.mixprec_a_quantizer.effective_precision
         cost = self.in_features * self.out_features * eff_w_prec * eff_a_prec
         return cost
+
+    @property
+    def out_features_eff(self) -> torch.Tensor:
+        """Returns the number of not pruned channels for this layer.
+
+        :return: the number of not pruned channels for this layer.
+        :rtype: torch.Tensor
+        """
+        if self.w_mixprec_type == MixPrecType.PER_CHANNEL:
+            return cast(torch.Tensor, self.mixprec_w_quantizer.out_features_eff)
+        else:
+            return cast(torch.Tensor, self.out_features)
+
+    @property
+    def input_features_calculator(self) -> FeaturesCalculator:
+        """Returns the `FeaturesCalculator` instance that computes the number of input features for
+        this layer.
+
+        :return: the `FeaturesCalculator` instance that computes the number of input features for
+        this layer.
+        :rtype: FeaturesCalculator
+        """
+        return self._input_features_calculator
+
+    @input_features_calculator.setter
+    def input_features_calculator(self, calc: FeaturesCalculator):
+        """Set the `FeaturesCalculator` instance that computes the number of input features for
+        this layer.
+
+        :param calc: the `FeaturesCalculator` instance that computes the number of input features
+        for this layer
+        :type calc: FeaturesCalculator
+        """
+        calc.register(self)
+        self._input_features_calculator = calc

plinio/methods/mixprec/nn/mixprec_qtz.py
@@ -60,6 +60,8 @@ class MixPrec_Qtz_Channel(nn.Module):
                  hard_softmax: bool = False,
                  disable_sampling: bool = False):
         super(MixPrec_Qtz_Channel, self).__init__()
+        if len(precisions) != len(set(precisions)):
+            raise ValueError("Precisions cannot be repeated")
         self.precisions = precisions
         self.cout = cout
         self.quantizer = quantizer
@@ -67,8 +69,6 @@ class MixPrec_Qtz_Channel(nn.Module):
         # NAS parameters
         self.alpha_prec = nn.Parameter(torch.Tensor(len(precisions), cout))
         self.alpha_prec.data.fill_(1.)  # Initially each precision is equiprobable
-        self.register_buffer('theta_alpha', torch.tensor(len(precisions), dtype=torch.float32))
-        self.theta_alpha.data = self.alpha_prec
         # Mixed-Precision Quantizers
         self.mix_qtz = nn.ModuleList()
         for p in precisions:
@@ -77,10 +77,35 @@ class MixPrec_Qtz_Channel(nn.Module):
             self.mix_qtz.append(qtz)
 
         # Init temperature to std value
-        self.register_buffer('temperature', torch.tensor(1., dtype = torch.float32))
+        self.register_buffer('temperature', torch.tensor(1., dtype=torch.float32))
         self.temperature = cast(torch.Tensor, self.temperature)
         self.update_softmax_options(gumbel_softmax, hard_softmax, disable_sampling)
+        self.register_buffer('theta_alpha', torch.tensor(len(precisions), dtype=torch.float32))
+        with torch.no_grad():
+            self.theta_alpha.data = self.alpha_prec
+            self.sample_alpha()
+        self.register_buffer('out_features_eff', torch.tensor(self.cout, dtype=torch.float32))
+        self.zero_index = None
+        for i, p in enumerate(self.precisions):
+            if p == 0:
+                self.zero_index = i
+                break
 
+    @property
+    def features_mask(self) -> torch.Tensor:
+        """Return the binarized mask that specifies which output features (channels) are kept by
+        the NAS
+
+        :return: the binarized mask over the features axis
+        :rtype: torch.Tensor
+        """
+        with torch.no_grad():
+            if self.zero_index is not None:
+                # extract the row corresponding to 0-bit precision from the one-hot argmax matrix
+                # of size (n_prec, cout), and do a 1s complement
+                return 1 - STEArgmax.apply(self.theta_alpha)[self.zero_index, :]
+            else:
+                return torch.ones((self.cout,))
 
     def sample_alpha_sm(self):
         """
@@ -98,10 +123,10 @@ class MixPrec_Qtz_Channel(nn.Module):
         """
         if self.training:
             self.theta_alpha = nn.functional.gumbel_softmax(
-                    logits = self.alpha_prec,
-                    tau = self.temperature.item(),
-                    hard = self.hard_softmax,
-                    dim = 0)
+                logits=self.alpha_prec,
+                tau=self.temperature.item(),
+                hard=self.hard_softmax,
+                dim=0)
         else:
             self.sample_alpha_sm()
 
@@ -131,7 +156,6 @@ class MixPrec_Qtz_Channel(nn.Module):
         else:
             self.sample_alpha = self.sample_alpha_sm
 
-
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         """The forward function of the searchable mixed-precision layer.
 
@@ -151,6 +175,13 @@ class MixPrec_Qtz_Channel(nn.Module):
             theta_alpha_i = self.theta_alpha[i].view((self.cout,) + (1,) * len(input.shape[1:]))
             y.append(theta_alpha_i * quantizer(input))
         y = torch.stack(y, dim=0).sum(dim=0)
+
+        # compute number of not pruned channels
+        if self.zero_index is not None:
+            self.out_features_eff = torch.sum(self.theta_alpha[self.zero_index, :])
+        else:
+            self.out_features_eff = torch.tensor(self.cout, dtype=torch.float32)
+
         return y
 
     @property
@@ -200,8 +231,6 @@ class MixPrec_Qtz_Layer(nn.Module):
         # NAS parameters
         self.alpha_prec = nn.Parameter(torch.Tensor(len(precisions)))
         self.alpha_prec.data.fill_(1.)  # Initially each precision is equiprobable
-        self.register_buffer('theta_alpha', torch.tensor(len(precisions), dtype=torch.float32))
-        self.theta_alpha.data = self.alpha_prec
         # Mixed-Precision Quantizers
         self.mix_qtz = nn.ModuleList()
         for p in precisions:
@@ -213,7 +242,10 @@ class MixPrec_Qtz_Layer(nn.Module):
         self.register_buffer('temperature', torch.tensor(1.))
         self.temperature = cast(torch.Tensor, self.temperature)
         self.update_softmax_options(gumbel_softmax, hard_softmax, disable_sampling)
-
+        self.register_buffer('theta_alpha', torch.tensor(len(precisions), dtype=torch.float32))
+        with torch.no_grad():
+            self.theta_alpha.data = self.alpha_prec
+            self.sample_alpha()
 
     def sample_alpha_sm(self):
         """
@@ -224,7 +256,6 @@ class MixPrec_Qtz_Layer(nn.Module):
         if self.hard_softmax:
             self.theta_alpha = STEArgmax.apply(self.theta_alpha)
 
-
     def sample_alpha_gs(self):
         """
         Samples the alpha architectural coefficients using a Gumbel SoftMax (with temperature).
@@ -232,10 +263,10 @@ class MixPrec_Qtz_Layer(nn.Module):
         """
         if self.training:
             self.theta_alpha = nn.functional.gumbel_softmax(
-                    logits = self.alpha_prec,
-                    tau = self.temperature.item(),
-                    hard = self.hard_softmax,
-                    dim = 0)
+                logits=self.alpha_prec,
+                tau=self.temperature.item(),
+                hard=self.hard_softmax,
+                dim=0)
         else:
             self.sample_alpha_sm()
 